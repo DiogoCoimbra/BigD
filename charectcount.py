0abb3eb78cc4dd19f4dbd1f26137ecd6ca4c8480
Diogo Coimbra <90319728+DiogoCoimbra@users.noreply.github.com>
2022-02-12 20:55:19 +0000

    Update charectcount.py

--- charectcount.py
+++ charectcount.py
@@ -1,5 +1,14 @@
 import sys
-from collections import Counter
 
-counter = Counter(sys.stdin)
-print counter[sys.stdin]
+for line in sys.stdin:
+  line = line.strip("\n")
+
+
+  words = line.split()
+  number_of_lines += 1
+  number_of_words += len(words)
+  number_of_characters += len(line)
+
+file.close()
+
+print("lines:", number_of_lines, "words:", number_of_words, "characters:", number_of_characters)
