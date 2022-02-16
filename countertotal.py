ace36d9ebb379da984491b034c9e78e4077531af
Diogo Coimbra <90319728+DiogoCoimbra@users.noreply.github.com>
2022-02-16 20:08:37 +0000

    Update and rename counters211.py to countertotal.py

---
+++ countertotal.py
@@ -0,0 +1,15 @@
+import sys
+
+
+TAB_CHAR = '\t'
+
+for line in sys.stdin:
+    for token in line.strip().split(" "):
+        if token: 
+            print("line" + TAB_CHAR + '1')
+    for token2 in line.split("\n")
+        if token2:
+            print('word' + TAB_CHAR + '1')
+    for token3 in line)
+        if token3:
+            print('Characters' + TAB_CHAR + '1')
