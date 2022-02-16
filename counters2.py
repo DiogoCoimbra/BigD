9a102bf4d459c60844da3cc49d4acc8a4404ccc1
Diogo Coimbra <90319728+DiogoCoimbra@users.noreply.github.com>
2022-02-16 19:04:52 +0000

    Update and rename counters22222.py to counters2.py

---
+++ counters2.py
@@ -0,0 +1,16 @@
+import sys
+
+
+TAB_CHAR = '\t'
+
+for line in sys.stdin:
+    for token in line.strip().split(" "):
+        if token: 
+            println(token + TAB_CHAR + '1')
+            for token2 in line.strip("\n"):
+                if token2:
+                    println(token2 + TAB_CHAR + '1')
+                        for token3 in line.strip().split():
+                            if token3:
+                                println(token3 + TAB_CHAR + '1')
+
