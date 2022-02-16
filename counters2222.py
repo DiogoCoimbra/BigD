d099e7625412066e03dbe05eef8e03a2818e5312
Diogo Coimbra <90319728+DiogoCoimbra@users.noreply.github.com>
2022-02-16 19:00:30 +0000

    Update counters2222.py

--- counters2222.py
+++ counters2222.py
@@ -6,10 +6,10 @@ TAB_CHAR = '\t'
 for line in sys.stdin:
     for token in line.strip().split(" "):
         if token: 
-            print(token + TAB_CHAR + '1')
+            println(token + TAB_CHAR + '1')
     for token2 in line.strip("\n"):
         if token2:
-            print(token2 + TAB_CHAR + '1')
+            println(token2 + TAB_CHAR + '1')
     for token3 in line.strip().split():
         if token3:
-            print(token3 + TAB_CHAR + '1')
+            println(token3 + TAB_CHAR + '1')
