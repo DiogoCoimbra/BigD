9923cea793609c2081bccf163701137a2cca774c
Diogo Coimbra <90319728+DiogoCoimbra@users.noreply.github.com>
2022-02-16 20:11:26 +0000

    Update countertotal.py

--- countertotal.py
+++ countertotal.py
@@ -7,9 +7,9 @@ for line in sys.stdin:
     for token in line.strip().split(" "):
         if token: 
             print("line" + TAB_CHAR + '1')
-    for token2 in line.split("\n")
-        if token2:
-            print('word' + TAB_CHAR + '1')
-    for token3 in line)
-        if token3:
-            print('Characters' + TAB_CHAR + '1')
+            for token2 in line.split("/n"):
+                if token2:
+                    print('word' + TAB_CHAR + '1')
+                    for token3 in line:
+                        if token3:
+                            print('Characters' + TAB_CHAR + '1')
