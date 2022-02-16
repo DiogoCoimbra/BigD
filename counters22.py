32a74bcd932ab46baeb2dfecb9f491890e59bf00
Diogo Coimbra <90319728+DiogoCoimbra@users.noreply.github.com>
2022-02-16 18:53:38 +0000

    Update and rename counter22.py to counters22.py

--- counters22.py
+++ counters22.py
@@ -7,9 +7,9 @@ for line in sys.stdin:
     for token in line.strip().split(" "):
         if token: 
             print(token + TAB_CHAR + '1')
-    for token2 in line.strip("\n")
+    for token2 in line.strip("\n"):
         if token2:
             print(token2 + TAB_CHAR + '1')
-    for token3 in len(line)
+    for token3 in len(line):
         if toke3:
             print(token3 + TAB_CHAR + '1')
